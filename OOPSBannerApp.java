3773c53c5eb78be1d4e7e6e11e2b5be6a9aa4cc1
kk3532-ops <kk3532@srmist.edu.in>
2026-02-21 14:39:00 +0530

    UC3 - OOPS Banner using String.join()

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,22 +1,52 @@
 /**
- * UC2 - Render OOPS as ASCII Banner
- * Description: Prints OOPS in 7-line ASCII format
- * Rules Followed:
- * - Used System.out.println()
- * - Used string concatenation (+)
- * - No loops used
+ * UC3 - Render OOPS as ASCII Banner using String.join()
  * Author: K. TEJA SAI K. RAMA KRISHNA
  */
+
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(" ***  " + " ***  " + "****  " + " **** ");
-        System.out.println("*   * " + "*   * " + "*   * " + "*     ");
-        System.out.println("*   * " + "*   * " + "*   * " + "*     ");
-        System.out.println("*   * " + "*   * " + "****  " + " ***  ");
-        System.out.println("*   * " + "*   * " + "*     " + "    * ");
-        System.out.println("*   * " + "*   * " + "*     " + "    * ");
-        System.out.println(" ***  " + " ***  " + "*     " + "****  ");
+        System.out.println(String.join("  ",
+                " *** ",
+                " *** ",
+                "**** ",
+                " ****"));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*    "));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*   *",
+                "*    "));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "**** ",
+                " *** "));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*    ",
+                "    *"));
+
+        System.out.println(String.join("  ",
+                "*   *",
+                "*   *",
+                "*    ",
+                "    *"));
+
+        System.out.println(String.join("  ",
+                " *** ",
+                " *** ",
+                "*    ",
+                "**** "));
     }
 }
